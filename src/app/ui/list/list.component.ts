90536eb0b7c6f088770a8fc5671a3d435c6a746d
Ksawery <pawlikksawery@gmail.com>
2022-10-17 10:37:58 +0200

    feature: added new component, form and specific invalids

---
+++ src/app/ui/list/list.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ListComponent {
+  readonly list: FormGroup = new FormGroup({
+    name: new FormControl(null, [Validators.required]),
+    age: new FormControl(null,[Validators.required]),
+    salary: new FormControl(null,[Validators.required]) });
+}
