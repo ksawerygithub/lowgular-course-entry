998f701e329fe3284d9168abc2c1eebba589d9a0
Ksawery <pawlikksawery@gmail.com>
2022-10-17 10:00:10 +0200

    feature: added validators

--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-employee-form',
@@ -9,8 +9,8 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class EmployeeFormComponent {
   readonly employeeForm: FormGroup = new FormGroup({
-    name: new FormControl("Ksawery"),
-    age: new FormControl(30) });
+    name: new FormControl(null, [Validators.required]),
+    age: new FormControl(null, [Validators.min(18),Validators.required]) });
 
 
 }
