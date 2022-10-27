109acfbefa82e97a2220c697443b7436e1c6e19d
Ksawery <pawlikksawery@gmail.com>
2022-10-27 17:47:59 +0200

    feature: create new model and service

---
+++ src/app/model/my-employee.model.ts
@@ -0,0 +1,7 @@
+export interface MyEmployeeModel {
+  readonly id: string;
+  readonly employee_name: string;
+  readonly employee_salary: string;
+  readonly employee_age: string;
+  readonly profile_img: "";
+}
---
+++ src/app/services/my-employee.service-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { MyEmployeeService } from './my-employee.service';
+
+@NgModule({
+  imports: [],
+  declarations: [],
+  providers: [MyEmployeeService],
+  exports: []
+})
+export class MyEmployeeServiceModule {
+}
