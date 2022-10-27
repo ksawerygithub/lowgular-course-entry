96ca2d4650d4daef84c8ff64ccef1832fd4e26aa
Ksawery <pawlikksawery@gmail.com>
2022-10-27 17:15:58 +0200

    feature: deleted services and models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmployeeListComponentModule } from "./ui/employee-list/employee-list.component-module";
-import {EmployeeFacesComponentModule} from "./ui/employee-faces/employee-faces.component-module";
-import {EmployeeService} from "./services/employee.service";
-import {EmployeeServiceModule} from "./services/employee.service-module";
-import {ProjectComponentModule} from "./ui/project/project.component-module";
-import {ProjectService} from "./services/project.service";
-import {ProjectServiceModule} from "./services/project.service-module";
-import {EmployeeFormComponentModule} from "./ui/employee-form/employee-form.component-module";
-import {ListComponentModule} from "./ui/list/list.component-module";
 
 
 @NgModule({
@@ -20,13 +11,6 @@ import {ListComponentModule} from "./ui/list/list.component-module";
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    EmployeeListComponentModule,
-    EmployeeFacesComponentModule,
-    EmployeeServiceModule,
-    ProjectServiceModule,
-    ProjectComponentModule,
-    EmployeeFormComponentModule,
-    ListComponentModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
