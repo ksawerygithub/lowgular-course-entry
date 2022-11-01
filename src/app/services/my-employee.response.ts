0d6cd8c8b73b41686dfed3bd312c6df9c3c3fd31
Ksawery <pawlikksawery@gmail.com>
2022-11-01 11:24:17 +0100

    new webstorm trial test

---
+++ src/app/services/my-employee.response.ts
@@ -0,0 +1,7 @@
+export interface MyEmployeeResponse {
+  readonly id: string;
+  readonly employee_name: string;
+  readonly employee_age: string;
+  readonly employee_salary: string;
+  readonly employee_img: '';
+}
